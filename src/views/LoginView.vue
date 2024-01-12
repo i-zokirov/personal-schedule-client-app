92f7fa383307b9a80f5534edaeea6a08e83aa2f1
Anorsoft <izokirov@outlook.com>
2024-01-12 21:30:03 +0500

    initial project set up

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <!-- Your login form goes here -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginView',
+    // Your component logic goes here
+}
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
