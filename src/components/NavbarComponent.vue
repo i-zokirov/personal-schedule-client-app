46b4889680fb53cf2600214654a60092f8330bdb
Anorsoft <izokirov@outlook.com>
2024-01-13 12:31:21 +0500

    fullcalendar integration

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,36 @@
+<script setup></script>
+
+<template>
+  <header class="bg-white">
+    <nav class="mx-auto flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <a href="#" class="-m-1.5 p-1.5">
+          <span class="sr-only">Schedule App</span>
+          <img
+            class="h-8 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt=""
+          />
+        </a>
+      </div>
+
+      <div class="">
+        <RouterLink to="/" class="text-sm font-semibold leading-6 text-gray-900"
+          >Calendar</RouterLink
+        >
+        <span class="mx-2"></span>
+        <RouterLink to="/locations" class="text-sm font-semibold leading-6 text-gray-900"
+          >Locations</RouterLink
+        >
+      </div>
+
+      <div class="lg:flex lg:flex-1 lg:justify-end">
+        <a href="#" class="text-sm font-semibold leading-6 text-gray-900"
+          >Log out <span aria-hidden="true">&rarr;</span></a
+        >
+      </div>
+    </nav>
+  </header>
+</template>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <script setup>
+import NavbarComponent from '@/components/NavbarComponent.vue'
 import { useAuthStore } from '@/stores/auth'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import FullCalendar from '@fullcalendar/vue3'
 import { useQuery } from '@vue/apollo-composable'
 import { gql } from 'graphql-tag'
-import { ref, watch } from 'vue'
+import { nextTick, ref, watch } from 'vue'
 
 const authStore = useAuthStore()
 const ALL_LOCATIONS_QUERY = gql`
@@ -15,6 +20,57 @@ const ALL_LOCATIONS_QUERY = gql`
   }
 `
 
+const FIND_MANY_EVENTS_QUERY = gql`
+  query FindManyEvents($input: FindManyArgsInput) {
+    events(findManyArgsInput: $input) {
+      data {
+        id
+        title
+        description
+        startDate
+        endDate
+        location {
+          id
+          name
+        }
+        createdBy {
+          id
+          firstName
+          lastName
+        }
+        createdAt
+        updatedAt
+      }
+      meta {
+        page
+        limit
+        total
+      }
+    }
+  }
+`
+
+const getEvents = (variables) => {
+  return useQuery(FIND_MANY_EVENTS_QUERY, variables, {
+    context: {
+      headers: {
+        Authorization: `Bearer ${authStore.token}`,
+      },
+    },
+  })
+}
+
+const variables = ref({
+  input: {
+    from: '2024-01-01',
+    to: '2024-01-31',
+    limit: 10,
+    page: 1,
+  },
+})
+
+const { result: eventsResult } = getEvents(variables.value)
+
 const { result } = useQuery(ALL_LOCATIONS_QUERY, null, {
   context: {
     headers: {
@@ -24,19 +80,91 @@ const { result } = useQuery(ALL_LOCATIONS_QUERY, null, {
 })
 
 const locations = ref([])
+const events = ref([])
+const calendarRef = ref(null)
 
-watch(result, (newVal, oldVal) => {
-  console.log('Result changed:', newVal, oldVal)
+watch(eventsResult, (newVal) => {
+  if (newVal && newVal.events) {
+    const formatted = newVal.events.data.map((event) => {
+      return {
+        ...event,
+        id: event.id,
+        title: event.title,
+        start: new Date(event.startDate),
+        end: new Date(event.endDate),
+      }
+    })
+
+    if (calendarRef.value) {
+      const calendarApi = calendarRef.value.getApi()
+      calendarApi.removeAllEventSources()
+      calendarApi.addEventSource(formatted)
+    }
+
+    nextTick(() => {
+      events.value = [...formatted]
+    })
+  }
+})
+
+watch(result, (newVal) => {
   if (newVal && newVal.locations) {
     locations.value = newVal.locations
   }
 })
+
+watch(events, (newVal) => {
+  if (newVal) {
+    console.log(newVal)
+  }
+})
+
+const calendarOptions = {
+  plugins: [
+    dayGridPlugin,
+    timeGridPlugin,
+    interactionPlugin, // needed for dateClick
+  ],
+  headerToolbar: {
+    center: 'dayGridMonth,timeGridWeek,timeGridDay',
+    right: 'prev,next,today',
+  },
+  initialView: 'dayGridMonth',
+  editable: true,
+  selectable: true,
+  eventDisplay: 'block',
+  eventResizableFromStart: true,
+}
 </script>
 
 <template>
   <main>
-    <div v-for="location in locations" :key="location.id">
-      {{ location.name }}
+    <NavbarComponent />
+    <div class="p-6 lg:px-8">
+      <FullCalendar ref="calendarRef" :options="calendarOptions">
+        <template v-slot:eventContent="arg">
+          <b>{{ arg.timeText }}</b>
+          <i>{{ arg.event.title }}</i>
+        </template>
+      </FullCalendar>
+
+      <div>
+        <h2 class="text-lg leading-6 font-medium text-gray-900">Locations</h2>
+        <div class="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
+          <div
+            v-for="location in locations"
+            :key="location.id"
+            class="bg-white overflow-hidden shadow rounded-lg"
+          >
+            <div class="px-4 py-5 sm:p-6">
+              <dl>
+                <dt class="text-sm font-medium text-gray-500 truncate">Name</dt>
+                <dd class="mt-1 text-sm text-gray-900 truncate">{{ location.name }}</dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
